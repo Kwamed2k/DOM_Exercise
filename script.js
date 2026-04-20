2e81055492c277e75626edd2667b756a7efac9ae
Kwamed2k <d2kiix@gmail.com>
2026-04-20 12:57:34 +0000

    update: typo errors comments and fix semicolomns issuses

--- script.js
+++ script.js
@@ -6,29 +6,29 @@ const container = document.querySelector("#container");
 // From the example
 const content = document.createElement("div");
 content.classList.add("content");
-content.textContent = "This is a glorious tex-content";
+content.textContent = "This is a glorious text-content";
 container.appendChild(content);
 
-// Task 1: <p> with red tex
+// Task 1: <p> with red text
 const redParagraph = document.createElement("p");
 redParagraph.style.color = "red";
-redParagraph.textContent = "Hi I'm red";
+redParagraph.textContent = "Hey I'm red!";
 container.appendChild(redParagraph);
 
 // Task 2: <h3> with blue text
 const blueText = document.createElement("h3");
 blueText.style.color = "blue";
-blueText.textContent = "Hey I'm a blue h3!"
+blueText.textContent = "Hey I'm a blue h3!";
 container.appendChild(blueText);
 
-// Task 3 a: <div> with nested <hi> and <p>
-const styleDiv = document.createElement("div")
+// Task 3 a: <div> with nested <h1> and <p>
+const styleDiv = document.createElement("div");
 styleDiv.style.border = "2px solid black";
 styleDiv.style.backgroundColor = "pink";
 
     // 3 b: Nested heading
     const h1Text = document.createElement("h1");
-    h1Text.textContent = "I'm in a div!"
+    h1Text.textContent = "I'm in a div!";
     styleDiv.appendChild(h1Text);
 
     // 3 c: Nested paragraph
